b210e58655ecc771fe8f14432238e277374eb579
M Aun Rasheed <greataun786@gmail.com>
2026-03-02 16:38:49 +0500

    feat; setup app folder structure

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from '../pages/Home'
+import MainLayout from '../layouts/MainLayout'
+
+const AppRouter = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path='' element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default AppRouter
